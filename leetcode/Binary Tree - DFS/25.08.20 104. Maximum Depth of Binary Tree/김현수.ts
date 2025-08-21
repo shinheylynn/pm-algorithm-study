af0c0e4e3bbce7679a66137ce73f0dd9df2bf6de
Hyeonsu Kim <hyeonsu.kim.dev@gmail.com>
2025-08-21 20:52:44 +0900

    fix: TreeNode 클래스 선언

--- leetcode/Binary Tree - DFS/25.08.20 104. Maximum Depth of Binary Tree/김현수.ts
+++ leetcode/Binary Tree - DFS/25.08.20 104. Maximum Depth of Binary Tree/김현수.ts
@@ -12,12 +12,23 @@
  * }
  */
 
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
 function maxDepth(root: TreeNode | null): number {
   if (!root) return 0;
 
   let answer = 1;
 
-  const inorder = (root: TreeNode | null, depth: number) => {
+  const inorder = (root, depth) => {
     if (!root) return;
 
     inorder(root.left, depth + 1);
