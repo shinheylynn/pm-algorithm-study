f324ae43ee335d305c88eecf264e26aab4583ae0
Hyeonsu Kim <hyeonsu.kim.dev@gmail.com>
2025-08-21 20:50:36 +0900

    add: 104. Maximum Depth of Binary Tree

---
+++ leetcode/Binary Tree - DFS/25.08.20 104. Maximum Depth of Binary Tree/김현수.ts
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function maxDepth(root: TreeNode | null): number {
+  if (!root) return 0;
+
+  let answer = 1;
+
+  const inorder = (root: TreeNode | null, depth: number) => {
+    if (!root) return;
+
+    inorder(root.left, depth + 1);
+    if (depth > answer) {
+      answer = depth;
+    }
+    inorder(root.right, depth + 1);
+  };
+
+  inorder(root, answer);
+
+  return answer;
+}
